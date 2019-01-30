a4fcc22e0bba23ab4e55fb868a2b6647709a0137
BT_1N3_30 <cpolisi6526@bths.edu>
2019-01-30 08:23:54 -0500

    some sorts wo binary

--- src/Challenges.java
+++ src/Challenges.java
@@ -1,3 +1,9 @@
 public class Challenges extends SortCompetition {
 
+    public static int challengeFour(int[][] arr){
+       /// Data Set - a multi-dimensional array int[1000][1000] all elements are random integers between 0-10000
+        // Task: Sort each sub-array and then sort the arrays by their median value
+        // Return the median of the median array
+
+    }
 }
---
+++ src/sorts.java
@@ -0,0 +1,40 @@
+public class sorts {
+    public static void selection(int[] arr) {
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            index = min(arr, i);
+            if (arr[i] > arr[index]) {
+                swap(arr, i, index);
+            }
+        }
+    }
+
+    public static void insertion(int[] arr) {
+        int min=min(arr,0);
+        swap(arr,0,min);
+        for(int i: arr){
+            for(int j=1;j<arr.length;j++){
+                if(arr[j-1]>arr[j]){
+                    swap(arr,j-1,j);
+                }
+            }
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        boolean swap = true;
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i]>arr[i+1]) {
+                    swap(arr, i, i + 1);
+
+                    swap = true;
+                }
+            }
+
+        }
+    }
+
+
+}
