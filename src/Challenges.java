0df4bc83c692d47fa9aae014374382d208b5cba0
BT_1N3_30 <cpolisi6526@bths.edu>
2019-02-01 08:36:50 -0500

    3 and min

--- src/Challenges.java
+++ src/Challenges.java
@@ -8,6 +8,11 @@ public class Challenges extends SortCompetition {
 
     }
 
+    public static int challengedThree(int[] arr){
+        sorts.insertion(arr);
+        return(sorts.findMed(arr));
+    }
+
     public static int challengeFour(int[][] arr){
        /// Data Set - a multi-dimensional array int[1000][1000] all elements are random integers between 0-10000
         // Task: Sort each sub-array and then sort the arrays by their median value
